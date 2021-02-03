087422098ad294b421644e825bb245de66584536
Tstudent2810 <sudharmobekti@gmail.com>
2021-02-03 07:20:41 +0000

    fork

--- penjumlahan.java
+++ penjumlahan.java
@@ -2,8 +2,8 @@ public class penjumlahan
 {
  public static void main (String [] args)
  {
-  int a = 4;
-  int b = 6;
+  int a = 125896574;
+  int b = 326583696;
   int c = a + b;
   System.out.println ("Hasil penjumlahannya adalah "+c);
  }
